94211e875cb4f2b0c31bb6a4687830a4ba90f0e7
rangsimant <hanwongsa.r@gmail.com>
2015-07-22 01:21:33 +0700

    update Facebook Redirect

--- postcenter/config/social.php
+++ postcenter/config/social.php
@@ -5,7 +5,7 @@ return [
 	'facebook' => [
 		'app_id' => '1059313717415435',
 		'app_secret' => 'aeae0c6e2ba3f32b3f5d2da93d8a9ea7',
-		'redirect' => 'http://dev.22webdev.com/postcenter/public/social/authfacebook',
+		'redirect' => 'http://localhost/website/postcenter/public/social/authfacebook',
 	],
 
 	// 'mandrill' => [
