60a820d26c7d04339e0712148fc9382d14bdac9f
rangsimant <hanwongsa.r@gmail.com>
2015-08-10 15:45:16 +0700

    refactor newAccountFacebook

--- SocialImage/app/config/facebook_config.php
+++ SocialImage/app/config/facebook_config.php
@@ -4,6 +4,11 @@ return array(
 
 	"app_id" => "1646024095622253",
 	"app_secret" => "25ad52f609d75e675d20704f44c3f7cf",
-	"app_token" => "1646024095622253|25ad52f609d75e675d20704f44c3f7cf"
+	"app_token" => "1646024095622253|25ad52f609d75e675d20704f44c3f7cf",
+
+	## Warroom App
+	"sub_app_id" => "571007986325514",
+	"sub_secret" => "f8f9a4b1bb7c1bf521b03f0aa2be45c6",
+	"sub_token" => "CAAIHVCgwWAoBANZCaaFsM4idWwskdNCieEchZALqP4lWdLSYRrWjjzEWJUoK8qflr17P3ukhObiCowjL8ef0NsOgRRZBkcVnclxqQELhpoqFh27VgMGnP6jpuBJVnAfodBvBQmWymq3pY2ASLlgcZCw7HCHpqqQPvLxm3FeykvLMnwABfdjy"
 
 	);
--- SocialImage/app/controllers/AccountController.php
+++ SocialImage/app/controllers/AccountController.php
@@ -15,10 +15,6 @@ class AccountController extends BaseController
 	public function postNewAccountFacebook()
 	{
 		$config = Config::get('facebook_config');
-
-		FacebookSession::setDefaultApplication($config['app_id'], $config['app_secret']);
-		$session = new FacebookSession($config['app_token']);
-
 		$input = Input::all();
 
 		$new_account = array();
@@ -26,23 +22,40 @@ class AccountController extends BaseController
 		{
 			try 
 			{
-				$request = new FacebookRequest($session, 'GET', '/'.$val.'?fields=name');
-				$response = $request->execute();
-				$graphObject = $response->getGraphObject();
-				$graphObject = $graphObject->asArray();
-
-				$new_account[$key]['account_subject'] = $input['subject'];
-				$new_account[$key]['account_id_user'] = $graphObject['id'];
-				$new_account[$key]['account_username'] = $graphObject['name'];
-				$new_account[$key]['account_channel'] = 'facebook';
-				$new_account[$key]['account_last_datetime'] = date('Y-m-d H:i:s',strtotime($input['since'][$key]));
+				$new_account[$key] = $this->getAccountFromFacebookAPI($val, $key, $config['app_id'], $config['app_secret'], $config['app_token'], $input);
 			} catch (Exception $e) 
 			{
-				return Redirect::to('admin')->with('message', $e->getMessage());
+				try 
+				{
+					$new_account[$key] = $this->getAccountFromFacebookAPI($val, $key, $config['sub_app_id'], $config['sub_secret'], $config['sub_token'], $input);
+
+				} catch (Exception $e) 
+				{
+					return Redirect::to('admin/page')->with('message', $e->getMessage());
+				}
 			}
 		}
 
 		Account::insert($new_account);
-		return Redirect::to('admin')->with('success', 'Add new page success.');
+		return Redirect::to('admin/page')->with('success', 'Add new page success.');
+	}
+
+	public function getAccountFromFacebookAPI($id_page, $idx, $app_id, $app_secret, $access_token, $input)
+	{
+		FacebookSession::setDefaultApplication($app_id, $app_secret);
+		$session = new FacebookSession($access_token);
+
+		$request = new FacebookRequest($session, 'GET', '/'.$id_page.'?fields=name');
+		$response = $request->execute();
+		$graphObject = $response->getGraphObject();
+		$graphObject = $graphObject->asArray();
+
+		$new_account['account_subject'] = $input['subject'];
+		$new_account['account_id_user'] = $graphObject['id'];
+		$new_account['account_username'] = $graphObject['name'];
+		$new_account['account_channel'] = 'facebook';
+		$new_account['account_last_datetime'] = date('Y-m-d H:i:s',strtotime($input['since'][$idx]));
+
+		return $new_account;
 	}
 }
