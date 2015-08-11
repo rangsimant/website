a892534be12dbd0ef8461e74ea4116cd8eac9ca7
rangsimant <hanwongsa.r@gmail.com>
2015-08-11 20:26:42 +0700

    edit theme login

--- postcenter/resources/views/auth/login.blade.php
+++ postcenter/resources/views/auth/login.blade.php
@@ -1,79 +1,157 @@
+
+
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <title>AdminLTE 2 | Log in</title>
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    @include('layout.header')
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta charset="utf-8" />
+    <title>POSTCenter @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="apple-touch-icon" href="{{ asset('image/POSTcenter.ico') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('image/POSTcenter.ico') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('image/POSTcenter.ico') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('image/POSTcenter.ico') }}">
+    <link rel="icon" href="{{ asset('image/POSTcenter.ico') }}" type="image/x-icon"/>
+    <link rel="shortcut icon" href="{{ asset('image/POSTcenter.ico') }}" type="image/x-icon"/>
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN Vendor CSS-->
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/pace/pace-theme-flash.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/boostrapv3/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/font-awesome/css/font-awesome.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-scrollbar/jquery.scrollbar.css') }}" rel="stylesheet" type="text/css" media="screen" />
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/bootstrap-select2/select2.css') }}" rel="stylesheet" type="text/css" media="screen" />
+    <link href="{{ asset('theme/theme_page/getting_started/html/assets/plugins/switchery/css/switchery.min.css') }}" rel="stylesheet" type="text/css" media="screen" />
+    <!-- BEGIN Pages CSS-->
+    <link href="{{ asset('theme/theme_page/getting_started/html/pages/css/pages-icons.css') }}" rel="stylesheet" type="text/css">
+    <link class="main-stylesheet" href="{{ asset('theme/theme_page/getting_started/html/pages/css/pages.css') }}" rel="stylesheet" type="text/css" />
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <link href="{{ asset('css/postcenter.css') }}" rel="stylesheet">
+    <!--[if lte IE 9]>
+        <link href="pages/css/ie9.css" rel="stylesheet" type="text/css" />
     <![endif]-->
-  </head>
-  <body class="login-page">
-    <div class="login-box">
-      <div class="login-logo">
-        <a href="../../index2.html"><b>POST</b>Center</a>
-      </div><!-- /.login-logo -->
-      <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-        @if (count($errors) > 0)
-			<div class="alert alert-danger">
-				<strong>Whoops!</strong> There were some problems with your input.<br><br>
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
-        <form role="form" method="POST" action="{{ url('/auth/login') }}">
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <div class="form-group has-feedback">
-            <input type="email" name="email" class="form-control" placeholder="Email"/>
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" name="password" class="form-control" placeholder="Password"/>
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <div class="row">
-            <div class="col-xs-8">    
-              <div class="checkbox icheck">
-                <label>
-                  <input type="checkbox" name="remember"> Remember Me
-                </label>
-              </div>                        
-            </div><!-- /.col -->
-            <div class="col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-            </div><!-- /.col -->
-          </div>
-        </form>
-        <div class="social-auth-links text-center">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
-        </div><!-- /.social-auth-links -->
-
-        <a href="{{ url('/password/email') }}">I forgot my password</a><br>
-
-      </div><!-- /.login-box-body -->
-    </div><!-- /.login-box -->
-
-    @include('layout.scripts')
     <script type="text/javascript">
-    $(function(){
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' // optional
-      });
+    window.onload = function()
+    {
+      // fix for windows 8
+      if (navigator.appVersion.indexOf("Windows NT 6.2") != -1)
+        document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="{{ asset('pages/css/windows.chrome.fix.css') }}" />'
+    }
+    </script>
+  </head>
+  <body class="fixed-header   ">
+    <!-- START PAGE-CONTAINER -->
+    <div class="login-wrapper ">
+      <!-- START Login Background Pic Wrapper-->
+      <div class="bg-pic">
+        <!-- START Background Pic-->
+        <img src="assets/img/demo/new-york-city-buildings-sunrise-morning-hd-wallpaper.jpg" data-src="assets/img/demo/new-york-city-buildings-sunrise-morning-hd-wallpaper.jpg" data-src-retina="assets/img/demo/new-york-city-buildings-sunrise-morning-hd-wallpaper.jpg" alt="" class="lazy">
+        <!-- END Background Pic-->
+        <!-- START Background Caption-->
+        <div class="bg-caption pull-bottom sm-pull-bottom text-white p-l-20 m-b-20">
+          <h2 class="semi-bold text-white">
+          Pages make it easy to enjoy what matters the most in the life</h2>
+          <p class="small">
+            images Displayed are solely for representation purposes only, All work copyright of respective owner, otherwise © 2013-2014 REVOX.
+          </p>
+        </div>
+        <!-- END Background Caption-->
+      </div>
+      <!-- END Login Background Pic Wrapper-->
+      <!-- START Login Right Container-->
+      <div class="login-container bg-white">
+        <div class="p-l-50 m-l-20 p-r-50 m-r-20 p-t-50 m-t-30 sm-p-l-15 sm-p-r-15 sm-p-t-40">
+          <img src="assets/img/logo.png" alt="logo" data-src="assets/img/logo.png" data-src-retina="assets/img/logo_2x.png" width="78" height="22">
+          <p class="p-t-35">Sign into your pages account</p>
+          @if (count($errors) > 0)
+      <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+          <!-- START Login Form -->
+          <form role="form" method="POST" action="{{ url('/auth/login') }}">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <!-- START Form Control-->
+            <div class="form-group form-group-default">
+              <label>Login</label>
+              <div class="controls">
+                <input type="text" name="email" placeholder="Username or Email" class="form-control" required>
+              </div>
+            </div>
+            <!-- END Form Control-->
+            <!-- START Form Control-->
+            <div class="form-group form-group-default">
+              <label>Password</label>
+              <div class="controls">
+                <input type="password" class="form-control" name="password" placeholder="Credentials" required>
+              </div>
+            </div>
+            <!-- START Form Control-->
+            <div class="row">
+              <div class="col-md-6 no-padding">
+                <div class="checkbox ">
+                  <input type="checkbox" value="1" id="checkbox1" name="remember">
+                  <label for="checkbox1">Keep Me Signed in</label>
+                </div>
+              </div>
+              <div class="col-md-6 text-right">
+                <a href="#" class="text-info small">Help? Contact Support</a>
+              </div>
+            </div>
+            <!-- END Form Control-->
+            <button class="btn btn-primary btn-cons m-t-10" type="submit">Sign in</button>
+          </form>
+          <!--END Login Form-->
+          <div class="pull-bottom sm-pull-bottom">
+            <div class="m-b-30 p-r-80 sm-m-t-20 sm-p-r-15 sm-p-b-20 clearfix">
+              <div class="col-sm-3 col-md-2 no-padding">
+                <img alt="" class="m-t-5" data-src="assets/img/demo/pages_icon.png" data-src-retina="assets/img/demo/pages_icon_2x.png" height="60" src="assets/img/demo/pages_icon.png" width="60">
+              </div>
+              <div class="col-sm-9 no-padding m-t-10">
+                <p><small>
+                Create a pages account. If you have a facebook account, log into it for this process. Sign in with <a href="#" class="text-info">Facebook</a> or <a href="#" class="text-info">Google</a></small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- END Login Right Container-->
+    </div>
+    <!-- END PAGE CONTAINER -->
+    <!-- BEGIN VENDOR JS -->
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/pace/pace.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery/jquery-1.11.1.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/modernizr.custom.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-ui/jquery-ui.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/boostrapv3/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery/jquery-easy.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-unveil/jquery.unveil.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-bez/jquery.bez.min.js') }}"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-ios-list/jquery.ioslist.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-actual/jquery.actual.min.js') }}"></script>
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <!-- END VENDOR JS -->
+    <!-- BEGIN CORE TEMPLATE JS -->
+    <script src="{{ asset('theme/theme_page/getting_started/html/pages/js/pages.js') }}" type="text/javascript"></script>
+    <!-- END CORE TEMPLATE JS -->
+    <!-- BEGIN PAGE LEVEL JS -->
+    <script src="{{ asset('theme/theme_page/getting_started/html/assets/js/scripts.js') }}" type="text/javascript"></script>
+    <!-- END PAGE LEVEL JS -->
+    <script>
+    $(function()
+    {
+      $('#form-login').validate()
     })
     </script>
-     
   </body>
 </html>
