c377e900e251241a340e9a22bf3c02e52d0b272c
rangsimant <hanwongsa.r@gmail.com>
2015-09-02 22:54:03 +0700

    edit logo login page

--- SocialImage/app/views/layout/user.blade.php
+++ SocialImage/app/views/layout/user.blade.php
@@ -6,7 +6,7 @@
 
 	<title>SOCIAL IMAGE</title>
 	<meta name="Rangimant Hanwongsa" content="Social Image">
-  	<link rel="shortcut icon" type="image/png" href="{{ asset('image/socialimage_logo.ico') }}"/>
+  	<link rel="shortcut icon" type="image/png" href="{{ asset('image/social_image.ico') }}"/>
 
 	<link rel="stylesheet" type="text/css" href="{{ asset('bootstrap-3.3.4/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('font-awesome-4.3.0/css/font-awesome.min.css') }}">
--- SocialImage/app/views/user/login.blade.php
+++ SocialImage/app/views/user/login.blade.php
@@ -1,9 +1,6 @@
 @extends('layout.user')
 
 @section('content')
-  <div class="text-center">
-    <img src="{{ URL::to('/image/original/social_image.png') }}" width="300">
-  </div>
       <form class="form-signin bg-login"  role="form" method="POST" action="{{{ URL::to('/users/login') }}}" accept-charset="UTF-8">
       	@if (Session::get('error'))
             <div class="alert alert-error alert-danger">{{{ Session::get('error') }}}</div>
@@ -12,7 +9,10 @@
         @if (Session::get('notice'))
             <div class="alert">{{{ Session::get('notice') }}}</div>
         @endif
-        <h2 class="form-signin-heading">Please sign in</h2>
+        <div class="text-center">
+          <img src="{{ URL::to('/image/original/social_image.png') }}" width="200">
+          <h2 class="form-signin-heading"> Please sign in</h2>
+        </div>
         <label for="inputEmail" class="sr-only">Email address</label>
         <input class="form-control" tabindex="1" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}"  required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
